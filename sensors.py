118ef10d74676198cd30f82c63c65183766bee67
weilinfox <fox@youmu.love>
2025-11-24 18:57:55 +0800

    sensor: independent TEMP class
    Signed-off-by: weilinfox <fox@youmu.love>

--- sensors.py
+++ sensors.py
@@ -8,6 +8,7 @@ from typing import Dict, List, Tuple
 class BAT:
     def __init__(self):
         # percent, secs_left, power_plugged
+        self.precent = False
         self.bat: Tuple[float, int, bool] = (100.0, 0, True)
 
         self.update()
@@ -15,10 +16,12 @@ class BAT:
     def update(self) -> None:
         b = psutil.sensors_battery()
         if b is not None:
+            self.precent = True
             self.bat = (b.percent, b.secsleft, b.power_plugged)
 
     def __str__(self) -> str:
-        return f"{self.bat}\n"
+        return (f"BAT Precent: {self.precent}\n"
+                f"BAT Data: {self.bat}\n")
 
 
 class CPU:
@@ -30,9 +33,6 @@ class CPU:
         self.cpu_freq = 0.0
         self.cpu_freq_core: List[float] = [0.0 for _ in range(self.cpu_count)]
 
-        self.cpu_temp_count: int = 0
-        self.cpu_temp: List[List[Tuple[str, float]]] = []
-
         self.update()
 
     def update(self) -> None:
@@ -41,39 +41,12 @@ class CPU:
         self.cpu_freq = psutil.cpu_freq(percpu=False).current
         self.cpu_freq_core = [ i.current for i in psutil.cpu_freq(percpu=True) ]
 
-        sys_base = pathlib.Path("/sys/class/hwmon")
-        if not sys_base.exists():
-            return
-        temp_count: int = 0
-        temp_list: List[List[Tuple[str, float]]] = []
-        for h in sys_base.glob("hwmon*/name"):
-            hn = h.read_bytes().decode(encoding="ascii").strip()
-            if hn not in ["k10temp", "coretemp"]:
-                continue
-            # there could be multiple cpu
-            temps: List[Tuple[str, float]] = []
-            for t in h.parent.glob("temp*_label"):
-                tl = t.read_bytes().decode(encoding="ascii").strip()
-                tf = t.parent.absolute() / ( t.name[:-5] + "input" )
-                if not tf.is_file():
-                    continue
-                tt = float(tf.read_bytes().decode(encoding="ascii").strip()) / 1000.0
-                temps.append( (tl, tt) )
-            if len(temps) > 0:
-                temp_count += 1
-                temp_list.append(temps)
-
-        self.cpu_temp_count = temp_count
-        self.cpu_temp = temp_list
-
     def __str__(self) -> str:
         return (f'CPU Cores: {self.cpu_count}\n'
                 f'CPU Usage: {self.cpu_usage}\n'
                 f'CPU Usage Core: {self.cpu_usage_core}\n'
                 f'CPU Frequency: {self.cpu_freq}\n'
-                f'CPU Frequency Core: {self.cpu_freq_core}\n'
-                f'CPU Socket Count: {self.cpu_temp_count}\n'
-                f'CPU Temperatures: {self.cpu_temp}\n')
+                f'CPU Frequency Core: {self.cpu_freq_core}\n')
 
 
 class FAN:
@@ -125,9 +98,6 @@ class DISK:
         self.rate_write = 0.0
         self.rate_read = 0.0
 
-        self.disk_count = 0
-        self.disk_temp: List[List[Tuple[str, float]]] = []
-
         self.update()
 
     def update(self) -> None:
@@ -144,41 +114,11 @@ class DISK:
         self.rate_write = (self.bytes_write - self.bytes_write_old) / (self.bytes_time - self.bytes_time_old)
         self.rate_read = (self.bytes_read - self.bytes_read_old) / (self.bytes_time - self.bytes_time_old)
 
-        # disk temperature sensors
-        disk_count = 0
-        disk_temp = []
-        sys_base = pathlib.Path("/sys/block")
-
-        if not sys_base.exists():
-            return
-        for d in sys_base.iterdir():
-            for s in d.glob("device/hwmon*"):
-                sensors = []
-                for t in s.glob("temp*_label"):
-                    if not t.is_file():
-                        continue
-                    sn = t.read_bytes().decode(encoding="ascii").strip()
-                    fi = t.parent.absolute() / ( t.name[:-5] + "input" )
-                    if not fi.is_file():
-                        continue
-                    si = float(fi.read_bytes().decode(encoding="ascii")) / 1000.0
-
-                    sensors.append( (sn, si) )
-
-                if len(sensors) > 0:
-                    disk_count += 1
-                    disk_temp.append(sensors)
-
-        self.disk_count = disk_count
-        self.disk_temp = disk_temp
-
     def __str__(self) -> str:
         return (f'Disk Bytes Write: {self.bytes_write}\n'
                 f'Disk Bytes Read: {self.bytes_read}\n'
                 f'Disk Bytes Write Rate: {self.rate_write}\n'
-                f'Disk Bytes Read Rate: {self.rate_read}\n'
-                f'Disk Count: {self.disk_count}\n'
-                f'Disk Temperatures: {self.disk_temp}\n')
+                f'Disk Bytes Read Rate: {self.rate_read}\n')
 
 
 class NET:
@@ -219,21 +159,101 @@ class NET:
 class TEMP:
     def __init__(self, fahrenheit: bool = False):
         # (name, label, current)
-        self.psutil_temp: Dict[str, List[Tuple[str, float]]] = {}
-        self.fahrenheit = fahrenheit
+        self.disk_count: int = 0
+        self.disk_names: List[str] = []
+        self.disk_temps: List[List[Tuple[str, float]]] = []
+        self.cpu_count: int = 0
+        self.cpu_names: List[str] = []
+        self.cpu_temps: List[Tuple[str, float]] = []
+        self.misc_count: int = 0
+        self.misc_names: List[str] = []
+        self.misc_temps: List[Tuple[str, float]] = []
 
         self.update()
 
     def update(self) -> None:
-        sensors = {}
 
-        for t in psutil.sensors_temperatures(fahrenheit=self.fahrenheit).items():
-            sensors[t[0]] = [ (f"id {k}" if t[1][k].label == "" else t[1][k].label, t[1][k].current) for k in range(len(t[1])) ]
+        temp_count: int = 0
+        dev_names: List[str] = []
+        temp_list: List[List[Tuple[str, float]]] = []
 
-        self.psutil_temp = sensors
+        # disk temperature sensors
+        sys_base = pathlib.Path("/sys/block")
+        if not sys_base.exists():
+            return
+        for d in sys_base.iterdir():
+            for s in d.glob("device/hwmon*"):
+                temps: List[Tuple[str, float]] = []
+                for t in s.glob("temp*_label"):
+                    if not t.is_file():
+                        continue
+                    sn = t.read_bytes().decode(encoding="ascii").strip()
+                    fi = t.parent.absolute() / ( t.name[:-5] + "input" )
+                    if not fi.is_file():
+                        continue
+                    si = float(fi.read_bytes().decode(encoding="ascii")) / 1000.0
+
+                    temps.append( (sn, si) )
+
+                if len(temps) > 0:
+                    temp_count += 1
+                    dev_names.append(d.name)
+                    temp_list.append(temps)
+
+        self.disk_count = temp_count
+        self.disk_names = dev_names
+        self.disk_temps = temp_list
+
+        # hardware temperature sensors
+        temp_count = 0
+        dev_names = []
+        temp_list = []
+        cpu_count: int = 0
+        cpu_names: List[str] = []
+        cpu_list: List[List[Tuple[str, float]]] = []
+        sys_base = pathlib.Path("/sys/class/hwmon")
+        if not sys_base.exists():
+            return
+        for h in sys_base.glob("hwmon*/name"):
+            hn = h.read_bytes().decode(encoding="ascii").strip()
+            temps: List[Tuple[str, float]] = []
+            for t in h.parent.glob("temp*_label"):
+                tl = t.read_bytes().decode(encoding="ascii").strip()
+                tf = t.parent.absolute() / ( t.name[:-5] + "input" )
+                if not tf.is_file():
+                    continue
+                tt = float(tf.read_bytes().decode(encoding="ascii").strip()) / 1000.0
+                temps.append( (tl, tt) )
+            if len(temps) > 0:
+                if hn in ["k10temp", "coretemp"]:
+                    # there could be multiple cpu
+                    cpu_count += 1
+                    cpu_names.append(hn)
+                    cpu_list.append(temps)
+                elif hn in ["nvme"]:
+                    pass
+                else:
+                    temp_count += 1
+                    dev_names.append(hn)
+                    temp_list.append(temps)
+
+        self.cpu_count = cpu_count
+        self.cpu_names = cpu_names
+        self.cpu_temps = cpu_list
+        self.misc_count = temp_count
+        self.misc_names = dev_names
+        self.misc_temps = temp_list
 
     def __str__(self) -> str:
-        return f"{self.psutil_temp}\n"
+        return (f"CPU Count {self.cpu_count}\n"
+                f"CPU Names: {self.cpu_names}\n"
+                f"CPU Temp: {self.cpu_temps}\n"
+                f"Disk Count: {self.disk_count}\n"
+                f"Disk Names: {self.disk_names}\n"
+                f"Disk Temp: {self.disk_temps}\n"
+                f"Misc Count: {self.misc_count}\n"
+                f"Misc Names: {self.misc_names}\n"
+                f"Misc Temp: {self.misc_temps}\n")
 
 
 if __name__ == "__main__":
